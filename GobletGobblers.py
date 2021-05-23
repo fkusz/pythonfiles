c9c78371ce7da60c1854bb81f0c01e6818a12eae
fkusz <83090704+fkusz@users.noreply.github.com>
2021-05-23 03:47:30 -0400

    Added AI

--- GobletGobblers.py
+++ GobletGobblers.py
@@ -1,129 +1,149 @@
-Square_State = [1,2,3,'a','b','c',[1,2],[1,3],[1,'b'],[1,'c'],['a',2],['a',3],
-                ['a','b'],['a','c'],[2,3],[2,'c'],['b',3],['b','c'],[1,2,3],
-                [1,2,'c'],[1,'b',3],[1,'b','c'],['a',2,3],['a',2,'c'],
-                ['a','b',3],['a','b','c']]
+import math
+from datetime import datetime
 
-theBoard = {'7': [' '] , '8': [' '] , '9': [' '] ,
-            '4': [' '] , '5': [' '] , '6': [' '] ,
-            '1': [' '] , '2': [' '] , '3': [' '] }
+class GamePiece: #Create the GamePiece class which can be used to create new pieces
+    def __init__(self,size,color,icon):
+        self.size  = size
+        self.color = color
+        self.icon  = icon
 
-print(len(theBoard['7']))
-board_keys = []
+######################## Set up the Game Board ################################
+N = GamePiece(0,'NULL',' ') # Create the NULL piece, which is useful for keeping functions consistent
+theBoard = {'a1': [N] , 'a2': [N] , 'a3': [N] ,
+            'b1': [N] , 'b2': [N] , 'b3': [N] ,
+            'c1': [N] , 'c2': [N] , 'c3': [N] , '0': [N] } # position 0 is "off the board"
+def printBoard(board):
+    print('\n')
+    print('  1   2   3' )
+    print('a ' + board['a1'][-1].icon + ' | ' + board['a2'][-1].icon + ' | ' + board['a3'][-1].icon)
+    print(' ---+---+---')
+    print('b ' + board['b1'][-1].icon + ' | ' + board['b2'][-1].icon + ' | ' + board['b3'][-1].icon)
+    print(' ---+---+---')
+    print('c ' + board['c1'][-1].icon + ' | ' + board['c2'][-1].icon + ' | ' + board['c3'][-1].icon)
+    
+board_keys = [] #This will be used later to wipe the board in the event of a replay
 for key in theBoard:
     board_keys.append(key)
+###############################################################################
+####################### Handle pieces and the board easier ####################
+def BoardColor(KEY):
+    return theBoard[KEY][-1].color
 
-def printBoard(board):
-    print(' ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'])
-    print('---+---+---')
-    print(' ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'])
-    print('---+---+---')
-    print(' ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'])
+def keycheck(KEY):
+    if theBoard.get(KEY) or KEY == '':
+        return True
+    print("That's not a valid space!")
+    return False
 
-colors = ['White','Black']
-Sizes = [1,2,3]
-class GamePiece:
-    def __init__(self,size,color):
-        self.size = size
-        self.color = color
-
-w1=0
-w2=0
-w3=0
-b1=0
-b2=0
-b3=0
+def getpiece(color, size = None, FROM = None): # return (Piece, Index)
+    if FROM == None:
+        UNUSED_PIECES = len(theBoard['0'])
+        for item in range(UNUSED_PIECES):
+            if (color == theBoard['0'][item].color)\
+            and (size == theBoard['0'][item].size):
+                    PlayPiece = theBoard['0'][item]
+                    return PlayPiece, item
+        return None, None
+    else:
+        return theBoard[FROM][-1] , -1
     
-Pieces = [GamePiece(size,color) for size in Sizes for color in colors]
-Labels = [w1,w2,w3,b1,b2,b3]
-w1 = Pieces[0]
-print(vars(w1))
+def CheckWin(color): #We check each color individually, because a player can win on a turn that isn't theirs.
+    if (BoardColor('a1') == BoardColor('a2') == BoardColor('a3') == color)\
+    or (BoardColor('b1') == BoardColor('b2') == BoardColor('b3') == color)\
+    or (BoardColor('c1') == BoardColor('c2') == BoardColor('c3') == color)\
+    or (BoardColor('c1') == BoardColor('b1') == BoardColor('a1') == color)\
+    or (BoardColor('c2') == BoardColor('b2') == BoardColor('a2') == color)\
+    or (BoardColor('c3') == BoardColor('b3') == BoardColor('a3') == color)\
+    or (BoardColor('a1') == BoardColor('b2') == BoardColor('c3') == color)\
+    or (BoardColor('c1') == BoardColor('b2') == BoardColor('a3') == color):
+        return True
+    return False
+###############################################################################
+######################## Generate all of the game pieces ######################
+def generate_pieces():
+    Colors = ['Blue','Black']
+    Sizes = [1,2,3]
+    Icons = ['1','2','3','A','B','C']        
+    for i in range(6): # 6 = len(Icons)
+            size  = Sizes[i%3]
+            color = Colors[(math.floor(i/3))%2]
+            icon  = Icons[i%6]
+            GAME_PIECE_TEMP = GamePiece(size, color, icon)
+            theBoard['0'].append(GAME_PIECE_TEMP) #Create two of the same pieces.
+            theBoard['0'].append(GAME_PIECE_TEMP) #Create two of the same pieces.
+    theBoard['0'].pop(0)
+###############################################################################
 
 def game():
-
-    turn = 'X'
+    generate_pieces()
+    game_over = False
+    turn = 'Blue'
     count = 0
 
-
-    for i in range(10):
+    while game_over == False:
+        origin = ''
+        size = None
+        move = None
         printBoard(theBoard)
-        print("It's your turn, " + turn + " Move which piece?")
-
-        FROM = input()
-        print("It's your turn, " + turn + " Move to which place?")
-
-        move = input()      
-        size = 1
-
-        if theBoard[move] == [' ']:
-            theBoard[move][0] == turn
-            count += 1
-        elif theBoard[move][0] < size:
-            theBoard[move].append(turn)
-            count += 1
-        else:
-            print("You can't put that there!.\nMove to which place?")
+        
+        #Ask which piece to move
+        if count > 1: # Only ask if a piece should be moved after both players have had a turn
+            origin = input(turn + ", if you want to move a piece, where from?\n(Just press enter if you do not want to move a piece)\n")
+            if not keycheck(origin):
+                continue
+            
+        #Ask where to move the piece
+        move = input("It's your turn, " + turn + ", Move to which place?\n")
+        if not keycheck(move):
+                continue
+            
+        #Ask what size piece to move    
+        if origin == '': #If no origin space is given, we need to ask what size piece should be played
+            origin = '0'
+            size = int(input("What size piece?\n"))
+            Play, Index = getpiece(turn,size)
+        else: #Otherwise we don't ask a size, we play the top piece from the origin space
+            Play, Index = getpiece(turn, FROM = origin)
+            if Play.color != turn: # Check if the piece is the player's color
+                print("That's not your piece! You can't move that!")
+                continue
+        
+        #Handle the piece we found
+        if Play == None: #Stop the player if he tries to play a 3rd new piece of a size
+            print("You don't have any more pieces of that size!")
             continue
+        else: #Play the piece. this branch removes that piece from where it was and places it in a new spot.
+            if theBoard[move][-1].size < Play.size:
+                theBoard[origin].pop(Index)
+                theBoard[move].append(Play)
+                count += 1
+            else:
+                print("You can't put that there! Your piece isn't large enough!")
+                continue
 
-        # Now we will check if player X or O has won,for every move after 5 moves. 
+        #Check both colors for win conditions. Call it a tie if both win simultaneously. 
         if count >= 5:
-            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
+            Blackwins = CheckWin('Black')
+            Bluewins  = CheckWin('Blue')
+            if Blackwins or Bluewins:
+                game_over = True
+                winner = 'Black' if Blackwins else 'Blue'
                 printBoard(theBoard)
                 print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")                
+                if (Blackwins and Bluewins):
+                    print(" **** It's a tie! ****")
+                    break
+                print(" **** " +winner + " won. ****")        
                 break
-            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': # across the middle
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': # across the bottom
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break 
-            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break 
 
-        # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
-        if count == 9:
-            print("\nGame Over.\n")                
-            print("It's a Tie!!")
-
-        # Now we have to change the player after every move.
-        if turn =='X':
-            turn = 'O'
-        else:
-            turn = 'X'        
+        #Change the player after each move.
+        turn = 'Black' if (turn == 'Blue') else 'Blue'   
     
-    # Now we will ask if player wants to restart the game or not.
-    restart = input("Do want to play Again?(y/n)")
-    if restart == "y" or restart == "Y":  
+    #Ask if player wants to restart the game or not.
+    restart = input("Do want to play Again?(y/n)\n")
+    if restart == "y" or restart == "Y":
         for key in board_keys:
-            theBoard[key] = " "
-
+            theBoard[key] = [N]
         game()
 
 if __name__ == "__main__":
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,111 +1,165 @@
-#Implementation of Two Player Tic-Tac-Toe game in Python.
+from datetime import datetime
+#It's starting to seem like a dictionary is a BAD way to code tic tac toe
+theBoard = {'a1': ' ' , 'a2': ' ' , 'a3': ' ' ,
+            'b1': ' ' , 'b2': ' ' , 'b3': ' ' ,
+            'c1': ' ' , 'c2': ' ' , 'c3': ' ' }
 
-''' We will make the board using dictionary 
-    in which keys will be the location(i.e : top-left,mid-right,etc.)
-    and initialliy it's values will be empty space and then after every move 
-    we will change the value according to player's choice of move. '''
-
-theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
-            '4': ' ' , '5': ' ' , '6': ' ' ,
-            '1': ' ' , '2': ' ' , '3': ' ' }
-
-board_keys = []
-
-for key in theBoard:
-    board_keys.append(key)
-
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
+# I populated board_keys in a specific order, starting with spaces-
+# having the highest utility and ending with the least.
+# This will be helpful in the event of alpha-beta pruning
+board_keys = ['b2','a1','a3','c1','c3','a2','b1','b3','c2']
+# for key in theBoard:
+#     board_keys.append(key)
 
 def printBoard(board):
-    print(' ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'])
-    print('---+---+---')
-    print(' ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'])
-    print('---+---+---')
-    print(' ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'])
+    print('\n')
+    print('  1   2   3' )
+    print('a ' + board['a1'] + ' | ' + board['a2'] + ' | ' + board['a3'])
+    print(' ---+---+---')
+    print('b ' + board['b1'] + ' | ' + board['b2'] + ' | ' + board['b3'])
+    print(' ---+---+---')
+    print('c ' + board['c1'] + ' | ' + board['c2'] + ' | ' + board['c3'])
 
+def CheckWin(board):
+    if (board['a1'] == board['a2'] == board['a3'] != ' ')\
+    or (board['b1'] == board['b2'] == board['b3'] != ' ')\
+    or (board['c1'] == board['c2'] == board['c3'] != ' ')\
+    or (board['c1'] == board['b1'] == board['a1'] != ' ')\
+    or (board['c2'] == board['b2'] == board['a2'] != ' ')\
+    or (board['c3'] == board['b3'] == board['a3'] != ' ')\
+    or (board['a1'] == board['b2'] == board['c3'] != ' ')\
+    or (board['c1'] == board['b2'] == board['a3'] != ' '):
+        return True
+    return False
+
+# ALWAYS CheckWin(board) BEFORE CheckTie(board)
+# Did not use if count == 9 to check for a tie because I wanted this to be transferable
+# To games where a set number of turns don't cause a tie.
+def CheckTie(board):
+    for key in board_keys:
+        if board[key] == ' ':
+            return False
+    return True
+
+# Won't work for Alpha Beta Pruning
+# def minimax(board, depth, player):
+#     Scores = []
+#     Moves = validmoves(board, player)
+#     if depth == 0:
+#         return 0, None
+#     if CheckWin(board):
+#         return simplescore(board, switchplayers(player)), None
+#     for move in Moves:
+#         CopyBoard = board.copy()
+#         CopyBoard[move] = player
+#         Scores.append(minimax(CopyBoard, depth-1, switchplayers(player))[0])
+#     if player == 'X':
+#         Max = max(Scores)
+#         Move = Moves[Scores.index(Max)]
+#         return Max, Move
+#     else:
+#         Min = min(Scores)
+#         Move = Moves[Scores.index(Min)]
+#         return Min, Move
+
+###############################################################################
+#Need to rewrite how it works for Alpha Beta Pruning. This is that attempt
+def minimax(board, depth, player):
+    Moves = validmoves(board, player)
+    if depth == 0:
+        return 0, None
+    if CheckWin(board):
+        return simplescore(board, switchplayers(player)), None
+    if player == 'X':
+        Max = -100
+        for move in Moves:
+            CopyBoard = board.copy()
+            CopyBoard[move] = player
+            Score = minimax(CopyBoard, depth-1, switchplayers(player))[0]
+            if Score > Max:
+                Max = Score
+                Move = move
+        return Max, Move
+    else:
+        Min = 100
+        for move in Moves:
+            CopyBoard = board.copy()
+            CopyBoard[move] = player
+            Score = minimax(CopyBoard, depth-1, switchplayers(player))[0]
+            if Score < Min:
+                Min = Score
+                Move = move
+        return Min, Move
+###############################################################################
+def simplescore(board, player):
+    return 20 if player == 'X' else -20
+def switchplayers(player):
+    return 'O' if player == 'X' else 'X'    
+def validmoves(board, player):
+    validmoves = []
+    for key in board_keys:
+        if board[key] == ' ':
+            validmoves.append(key)
+    return validmoves
+def keycheck(KEY):
+    if theBoard.get(KEY):
+        return True
+    print("That's not a valid space!")
+    return False
 # Now we'll write the main function which has all the gameplay functionality.
 def game():
 
     turn = 'X'
     count = 0
 
-
-    for i in range(10):
+    for i in range(20):
         printBoard(theBoard)
-        print("It's your turn, " + turn + " Move to which place?")
-
-        move = input()        
-
-        if theBoard[move] == ' ':
-            theBoard[move] = turn
-            count += 1
+        if turn == 'O':
+            print("It's your turn, move to which place?")
+            move = input()        
+            if keycheck(move):
+                if theBoard[move] == ' ':
+                    theBoard[move] = turn
+                    count += 1
+                else:
+                    print("That place is already filled.\nMove to which place?")
+                    continue
+            else:
+                continue
         else:
-            print("That place is already filled.\nMove to which place?")
-            continue
-
+            Start = datetime.now()
+            score, bestmove = minimax(theBoard,9-count,turn)
+            Stop = datetime.now()
+            print(Stop-Start)
+            theBoard[bestmove] = turn
+            print("The AI moves to " + bestmove)
+            count += 1
         # Now we will check if player X or O has won,for every move after 5 moves. 
         if count >= 5:
-            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")                
-                break
-            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': # across the middle
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': # across the bottom
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
+            if CheckWin(theBoard):
                 printBoard(theBoard)
                 print("\nGame Over.\n")                
                 print(" **** " +turn + " won. ****")
                 break 
-            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
+            if CheckTie(theBoard):
                 printBoard(theBoard)
                 print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
+                print("It's a Tie!!")
                 break
-            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break 
-
-        # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
-        if count == 9:
-            print("\nGame Over.\n")                
-            print("It's a Tie!!")
-
+            
         # Now we have to change the player after every move.
-        if turn =='X':
-            turn = 'O'
-        else:
-            turn = 'X'        
+        turn = switchplayers(turn)        
     
     # Now we will ask if player wants to restart the game or not.
-    restart = input("Do want to play Again? (y/n)")
-    if restart == "y" or restart == "Y":  
-        for key in board_keys:
-            theBoard[key] = " "
-
-        game()
+    if count == 20 and not CheckTie(theBoard):
+        print("How'd you manage to type the wrong space that many times? Your game is canceled!")
+    else:
+        restart = input("Do want to play Again? (y/n)")
+        if restart == "y" or restart == "Y":  
+            for key in board_keys:
+                theBoard[key] = " "
+    
+            game()
 
 if __name__ == "__main__":
     game()
